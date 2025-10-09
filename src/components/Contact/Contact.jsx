53c51ec5b69d1532e54d36ea8ca70ef4ce92c745
Sebah7 <sebahismail21@gmail.com>
2025-10-09 13:46:21 +0200

    Initial React portfolio commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,28 @@
+import styles from "./Contact.module.css";
+import { MdEmail } from "react-icons/md";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <MdEmail size={24} />
+          <a href="mailto:sebahismail21@gmail.com">sebahismail21@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <FaLinkedin size={24} />
+          <a href="https://www.linkedin.com/in/sebah-ismail-abubeker-361633bb/">linkedin.com/sebah-ismail-abubeker</a>
+        </li>
+        <li className={styles.link}>
+          <FaGithub size={24} />
+          <a href="https://www.github.com/Seb018">github.com/Seb018</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
